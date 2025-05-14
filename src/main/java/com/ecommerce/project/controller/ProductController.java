cd39c6fc7999e2a956cbded159fc19bd4a870492
yiannikouros <yiannikp@gmail.com>
2025-05-13 22:27:37 -0400

    Adding Product classes part 2. **hot code**

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,9 +1,21 @@
 package com.ecommerce.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product, @PathVariable Long categoryId) {
+        ProductDTO productDTO = productService.addProduct(product, categoryId);
+        return null;
+    }
 }
--- src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -1,27 +1,18 @@
-package com.ecommerce.project.model;
+package com.ecommerce.project.payload;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+public class ProductDTO {
     private Long productId;
     private String productName;
     private String image;
-    private String description;
     private Integer quantity;
     private double price;
     private double discount;
     private double specialPrice;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
 }
